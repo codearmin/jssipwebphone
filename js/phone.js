cb8981f9bd57ffedc27c3273246c6653ada986ad
codearmin <74765010+codearmin@users.noreply.github.com>
2021-02-02 17:32:22 +0100

    Update phone.js

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,4 @@
-$("#callStatus").addClass('d-none');
-$("#callAnswer").addClass('d-none');
+$("#callStatus ,#callAnswer ,#hangUp ,#takeCall").addClass('d-none');
 document.getElementById("status").innerHTML = "not active";
 
 var ua;
@@ -7,7 +6,7 @@ var session;
 
 JsSIP.debug.enable('JsSIP:*'); // JsSIP Debugger
 
-// + + + + + Start Call Funktionen  + + + + + 
+// connect to the asterisk server
 var socket = new JsSIP.WebSocketInterface('wss://' + asteriskIp + ':8089/ws');
 configuration = {
     sockets:          [socket],
@@ -15,13 +14,16 @@ configuration = {
     'password':       asteriskUserPass,
 };
 
+// audioStream
 var remoteAudio = new window.Audio();
 remoteAudio.autoplay = true;
 
+// call options vor call() and answer()
 var options = {
     'mediaConstraints' : { 'audio': true, 'video': false }
 };
 
+// new user agent
 ua = new JsSIP.UA(configuration);
 
 ua.on('connected', function(e) {
@@ -40,53 +42,65 @@ ua.on('registrationFailed', function(e) {
     document.getElementById("user-status").innerHTML = "Registering on SIP server failed with error: " + e.cause;
 });
 
-ua.on('newRTCSession', function (e) { // new session for outbound and inbound calls
+// new session for outbound and inbound calls
+ua.on('newRTCSession', function (e) { 
     document.getElementById("status").innerHTML = "new rtc session";
-
+    
     var newSession = e.session;
     session = newSession;
 
+    // calling number for inbound call 
+    var remote_identity_uri = session.remote_identity.uri.toString() ;
+    var pos = remote_identity_uri.lastIndexOf('@');
+    remote_identity_uri = remote_identity_uri.slice(4, pos);
+
+    // call ended normally
     session.on('ended', function(e) {
         document.getElementById("status").innerHTML = "call ended";
-        $("#callControl").removeClass('d-none');
-        $("#callStatus").addClass('d-none'); 
-        $("#callAnswer").addClass('d-none');
+        $("#callControl ,#connectCall").removeClass('d-none'); // updateUI
+        $("#callStatus, #callAnswer, #hangUp").addClass('d-none');  // updateUI
         ringTone.pause();
         session = null;       
     });
 
+    // call failed
     session.on('failed', function(e) {
         document.getElementById("status").innerHTML = "call failed";
-        $("#callControl").removeClass('d-none');
-        $("#callStatus").addClass('d-none'); 
-        $("#callAnswer").addClass('d-none');
-        ringTone.pause();
+        $("#callControl, #connectCall").removeClass('d-none'); // updateUI
+        $("#callStatus, #callAnswer, #hangUp").addClass('d-none');  // updateUI
+        ringTone.pause(); // stop ringtone for inbound calls
         session = null;
     });
 
+    // call accepted
     session.on('accepted', function(e) {
-        document.getElementById("status").innerHTML = "call accepted";
-        ringTone.pause();
+        var starttime = session.start_time;
+        document.getElementById("status").innerHTML = starttime; // starttime fired after accepted
+        ringTone.pause(); // stop ringtone for inbound calls
     });
 
+    // audioStream for outbound call
+    session.on('connecting', audioStream);
+    // audioStream for inbound call
     session.on('peerconnection', audioStream);
 
+    // call confirmed
     session.on('confirmed', function(e) {
-        document.getElementById("status").innerHTML = "call confirmed";
-        ringTone.pause();
+        ringTone.pause(); // stop ringtone for inbound calls
     });
 
+    // incomming call
     if(session.direction == 'incoming') {
-        document.getElementById("status").innerHTML = "incomming call";
-        ringTone.play();
-        $("#callControl").addClass('d-none');
-        $("#callStatus").addClass('d-none');
-        $("#callAnswer").removeClass('d-none'); 
+        document.getElementById("incommingNumber").innerHTML = "<i class='fas fa-phone-volume'>&nbsp;&nbsp;&nbsp;</i>" + remote_identity_uri;
+        ringTone.play(); // stop ringtone for inbound alls
+        $("#callControl ,#callStatus, #connectCall ,#callingNumber").addClass('d-none'); // updateUI
+        $("#callAnswer ,#takeCall").removeClass('d-none'); // updateUI
     } 
 });
-
+// user agent start
 ua.start();
 
+// add audioStream
 function audioStream() {
     session.connection.ontrack = function(e) {
         remoteAudio.srcObject = e.streams[0];
@@ -96,24 +110,23 @@ function audioStream() {
 $('#connectCall').click(function () {
     let callingNumber = $('#callNumber').val();
     document.getElementById("callingNumber").innerHTML = callingNumber;
-    $("#callControl").addClass('d-none');
-    $("#callStatus").removeClass('d-none'); 
-    ua.call(callingNumber, options);
+    $("#callControl ,#connectCall").addClass('d-none'); // updateUI
+    $("#callStatus ,#hangUp").removeClass('d-none'); // updateUI
+    ua.call(callingNumber, options); // call()
 });
 
 $('#takeCall').click(function () {
     document.getElementById("status").innerHTML = "call accepted!";
-    $("#callControl").addClass('d-none');
-    $("#callStatus").removeClass('d-none'); 
-    $("#callAnswer").addClass('d-none');
-    session.answer(options);
-    ringTone.pause();
+    $("#hangUp").removeClass('d-none'); // updateUI
+    $("#takeCall").addClass('d-none'); // updateUI
+    session.answer(options); // answer()
+    ringTone.pause(); // stop ringtone for inbound calls
 });
 
 $('#hangUp').click(function () {
-    $("#callControl").removeClass('d-none');
-    $("#callStatus").addClass('d-none');
-    session.terminate();
+    $("#callControl ,#connectCall").removeClass('d-none'); // updateUI
+    $("#callStatus ,#hangUp").addClass('d-none'); // updateUI
+    session.terminate(); // terminate session
 });
 
 
